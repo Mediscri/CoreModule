6a610afc1dc503fbb4267f13de46c990f0d06674
Dongmin Kim <dmk98@naver.com>
2018-08-25 17:30:05 +0900

    [MED-40] refactor code.

--- wrapper.py
+++ wrapper.py
@@ -1,8 +1,6 @@
 # -*- coding:utf-8 -*-
-import numpy as np
 from konlpy.tag import Twitter
 from sklearn.externals import joblib
-import pandas as pd
 import json
 
 
@@ -29,26 +27,3 @@ class Classifier:
                                key=lambda x: -x[1])
         json_dict = {'deep_outputs': top_score_map, 'raw_sentence': text}
         return json.dumps(json_dict)
-
-
-clf = Classifier()
-Rbf_svm = clf
-
-data_df = pd.read_csv('total.csv', encoding='utf-8')
-labels, levels = pd.factorize(data_df['classification'])
-
-"""
-print(clf.pipe.score(data_df['tokens'], labels))
-print(clf.inference('배가 아파요'))
-print(Rbf_svm.inference('사실 요즘 배가 아파서 왔어요.'))
-print(Rbf_svm.inference('설사가 너무 심한거 같네요.'))
-print(Rbf_svm.inference('속이 계속 더부룩한 느낌이에요'))
-print(Rbf_svm.inference('명치 쪽이 계속 땡기는거 같아요'))
-print(Rbf_svm.inference('어제 속이 메스꺼워서 구토를 오랬동안 했던거 같아요'))
-"""
-
-print(clf.inference('사실 요즘 배가 아프고 구토가 심해서 왔어요.. 제가 너무 잘생겨서 그런것이겠죠 원지운님이 저를 보고 웃고있어요 김동민 화이팅'))
-print(clf.inference('어젯밤에 설사를 하다가 명치가 아팠어요.'))
-print(clf.inference('설사가 심한데 약을 먹어야 할까요?'))
-print(clf.inference('배에 가스가 찬 기분이에요.'))
-print(clf.inference('어머니가 위암으로 돌아가셨어요.'))
